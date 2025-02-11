37157d83eef28cf06d1ce2ad907620f7d9df982e
Yanhao Li <1310724226@qq.com>
2025-02-11 23:26:09 +0800

    250211

---
+++ 70.爬楼梯.py
@@ -0,0 +1,15 @@
+#
+# @lc app=leetcode.cn id=70 lang=python3
+#
+# [70] 爬楼梯
+#
+
+# @lc code=start
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        a, b = 1, 1
+        for _ in range(n - 1):
+            a, b = b, a + b
+        return b
+# @lc code=end
+
