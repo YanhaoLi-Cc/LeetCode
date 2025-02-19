da40566fe86056db6467ea91f156daf42365866b
Yanhao Li <1310724226@qq.com>
2025-02-19 14:07:35 +0800

    250219

--- 64.最小路径和.py
+++ 64.最小路径和.py
@@ -7,27 +7,16 @@
 # @lc code=start
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
-        # m, n = len(grid), len(grid[0])
-        # for i in range(m):
-        #     for j in range(n):
-        #         if i == 0 and j == 0:
-        #             continue
-        #         elif i == 0:
-        #             grid[i][j] += grid[i][j-1]
-        #         elif j == 0:
-        #             grid[i][j] += grid[i-1][j]
-        #         else:
-        #             grid[i][j] += min(grid[i-1][j], grid[i][j-1])
-        # return grid[m - 1][n - 1]
         m, n = len(grid), len(grid[0])
-        MAX = 1e8
-        dp = [[MAX] * (n + 1) for _ in range((m + 1))]
-        dp[0][1] = dp[1][0] = 0
-        
-        for i in range(m):
-            for j in range(n):
-                dp[i + 1][j + 1] = min(dp[i][j + 1], dp[i + 1][j]) + grid[i][j]
-        
-        return dp[m][n] 
+        dp = [[0] * n for _ in range(m)]
+        dp[0][0] = grid[0][0]
+        for i in range(1, n):
+            dp[0][i] = dp[0][i - 1] + grid[0][i]
+        for i in range(1, m):
+            dp[i][0] = dp[i - 1][0] + grid[i][0]
+        for i in range(1, m):
+            for j in range(1, n):
+                dp[i][j] = min(dp[i][j-1], dp[i-1][j]) + grid[i][j]
+        return dp[m-1][n-1]
 # @lc code=end
 
