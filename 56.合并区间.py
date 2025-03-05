b3c4809376ae6014810314b83fc7337e4245814d
Yanhao Li <1310724226@qq.com>
2025-03-05 16:11:47 +0800

    250305

--- 56.合并区间.py
+++ 56.合并区间.py
@@ -7,30 +7,17 @@
 # @lc code=start
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        # intervals.sort(key=lambda x: x[0])
-        # merged = []
-        # for interval in intervals:
-        #     if not merged:
-        #         merged.append(interval)
-        #     if merged[-1][1] < interval[0]:
-        #         merged.append(interval)
-        #     else:
-        #         merged[-1][1] = max(merged[-1][1], interval[1])
-        # return merged
         
         intervals.sort(key=lambda x: x[0])
-        merged = []
-        left, right = intervals[0][0], intervals[0][1]
-        
-        for interval in intervals:
-            if right >= interval[0]:
-                if right > interval[1]:
-                    continue
-                else:
-                    right = interval[1]
+
+        merged = [intervals[0]]
+
+        for interval in intervals[1:]:
+            if interval[0] > merged[-1][1]:
+                merged.append(interval)
             else:
-                merged.append([left, right])
-                left, right = interval[0], interval[1]
-        return merged + [[left, right]]
+                merged[-1][1] = max(merged[-1][1], interval[1])
+
+        return merged
 # @lc code=end
 
